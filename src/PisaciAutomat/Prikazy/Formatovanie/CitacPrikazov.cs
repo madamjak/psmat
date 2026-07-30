ce422ea97fcdde74451ce71ce20bc088aa56540f
madamjak <marian.adamjak@gmail.com>
2026-07-30 10:04:03 +0200

    jednoduche zalamovanie r (#100)

---
+++ src/PisaciAutomat/Prikazy/Formatovanie/CitacPrikazov.cs
@@ -0,0 +1,48 @@
+﻿using PisaciStroj.Lexer;
+using PisaciStroj.Pamat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PisaciAutomat.Prikazy.Formatovanie
+{
+    public static class CitacPrikazov
+    {
+        public static HashSet<string> VyhladavaciePrikazy = new HashSet<string>
+        {
+            "zari"
+        };
+
+        public static PrikazovyAutomatResult NacitajPrikaz(GapBuffer prikazovyRiadok, List<Token> tokeny, string typPrikazu)
+        {
+            var r = new PrikazovyAutomatResult();
+
+            var p = new Prikaz();
+
+            try
+            {
+                if (typPrikazu == "zari")
+                {
+                    if (!(tokeny.Count == 1))
+                    {
+                        return null;
+                    }
+
+                    p.Typ = TypPrikazu.ZalomRiadky;
+
+                    r.Prikaz = p;
+                    return r;
+                }
+                else
+                {
+                    return r;
+                }
+            }
+            catch
+            {
+                r.Prikaz = null;
+                return r;
+            }
+        }
+    }
+}
